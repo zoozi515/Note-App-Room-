90fa988e27946b59d669dc7db648c4cbdf256d35
Zahra Yousef <45607455+zoozi515@users.noreply.github.com>
2021-11-03 21:16:22 +0300

    Note App - Using Room - Version #5 -> one file is added

---
+++ app/src/main/java/com/example/w6_d4_note_app/NoteDao.kt
@@ -0,0 +1,18 @@
+package com.example.w6_d4_note_app
+
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+    @Query("SELECT * FROM Notes ORDER BY id ASC")
+    suspend fun viewNotes(): List<Notes>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addNote(note: Notes)
+
+    @Delete
+    suspend fun deleteNote(note: Notes)
+
+    @Update
+    suspend fun updateNote(note: Notes)
+}
